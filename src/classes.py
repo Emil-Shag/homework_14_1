804280e32d42944e2c66d2ba3496eac7b6ba5d5a
Emil <kitanarau@yandex.ru>
2026-01-16 00:31:42 +0500

    task 3 and 4 done

--- src/classes.py
+++ src/classes.py
@@ -10,9 +10,27 @@ class Product:
         """Метод для инициализации экземпляра класса"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, value):
+        if value > 0:
+            self.__price = value
+        else:
+            print("Цена не должна быть нулевая или отрицательная")
+
+    @classmethod
+    def new_product(cls, dictionary):
+        result = cls(name=dictionary['name'],
+            description=dictionary['description'],
+            price=dictionary['price'],
+            quantity=dictionary['quantity'])
+        return result
 
 class Category:
     """Класс для представления категорий"""
@@ -31,6 +49,9 @@ class Category:
         self.__products = []
 
         Category.category_count += 1
+        if products:
+            for product in products:
+                self.add_product(product)
 
     def add_product(self, product: Product):
         """Метод для добавления в список товаров объект Product"""
